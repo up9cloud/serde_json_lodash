12efb0e0e46745a940f7cd8d458d77d3ec7d775e
up9cloud <8325632+up9cloud@users.noreply.github.com>
2022-01-23 05:14:18 -0800

    build: bump type for bump_push.sh

--- bump_push.sh
+++ bump_push.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
 
+# major, minor, patch
+if [ -z "$1" ]; then
+	_type=patch
+fi
+
 # Similar as npm version, but without `v` prefix, e.q. npm: `v1.0.0` vs bump `1.0.0`
-cargo bump patch --git-tag
+cargo bump $_type --git-tag
 git push
 git push --tags
